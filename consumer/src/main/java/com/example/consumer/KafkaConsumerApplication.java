eb53d9ed0d86e995f1df0e6513d0a428fbfdd316
Evgeny Tkach <1eluzive1@gmail.com>
2021-10-08 19:55:05 +0300

    producer -> register(without validation)

--- consumer/src/main/java/com/example/consumer/KafkaConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/KafkaConsumerApplication.java
@@ -1,17 +1,28 @@
 package com.example.consumer;
 
 import com.example.consumer.data.Payment;
+import lombok.AllArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.lang.NonNull;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
+@AllArgsConstructor
 @SpringBootApplication
 public class KafkaConsumerApplication {
   private final Log logger = LogFactory.getLog(this.getClass());
+  private final KafkaTemplate<String, Payment> template;
 
   public static void main(String[] args) {
     SpringApplication.run(KafkaConsumerApplication.class, args);
@@ -20,6 +31,33 @@ public class KafkaConsumerApplication {
   @KafkaListener(groupId = "consumer", topics = "payments")
   public void listen(Payment message, ConsumerRecord<String, Payment> record, Acknowledgment acknowledgment) {
     logger.info(message);
+    //send(message);
+
     acknowledgment.acknowledge();
+
   }
+
+  public void send(Payment payment){
+    final ListenableFuture<SendResult<String, Payment>> future = template.send(
+            new ProducerRecord<>("featuredPayments", "ibank", payment
+                   ));
+    future.addCallback(new ListenableFutureCallback<>() {
+      @Override
+      public void onFailure(@NonNull Throwable e) {
+        e.printStackTrace();
+      }
+
+      @Override
+      public void onSuccess(SendResult<String, Payment> result) {
+        logger.info(result);
+      }
+    });
+  }
+
+
+  @Bean // Kafka Admin
+  public NewTopic messagesTopic() {
+    return new NewTopic("featuredPayments", 3, (short)2);
+  }
+
 }
